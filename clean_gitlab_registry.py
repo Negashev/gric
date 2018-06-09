9b9c2c359ae7d140ff0489434834caa9edde7bda
negashev.al <aleksandr.negashev@domru.ru>
2018-06-09 15:57:44 +0500

    Add single remove method

--- clean_gitlab_registry.py
+++ clean_gitlab_registry.py
@@ -38,8 +38,10 @@ if 'DRY_RUN' in os.environ:
 else:
     dry_run = False
 
+REGISTRY_DATA_DIR = os.getenv('REGISTRY_DATA_DIR',
+                                  "/var/opt/gitlab/gitlab-rails/shared/registry/docker/registry/v2")
 
-async def validate(request):
+async def batch_remove(request):
     if 'clean-token' in request.query and request.query['clean-token'] == token:
         project_namespace = request.match_dict['project_namespace']
         project_name = request.match_dict['project_name']
@@ -50,40 +52,53 @@ async def validate(request):
 
     return request.Response(text='request not valid')
 
+async def single_remove(request):
+    if 'clean-token' in request.query and 'path' in request.query and request.query['clean-token'] == token:
+        logger.info(f"Valid request, processing {request.query['path']}")
+        image, tag = request.query['path'].split(':')
+        remove(image, tag)
+        return request.Response(text='ok')
+
+    return request.Response(text='request not valid')
+
 
 def cleanup(project_namespace, project_name, tag):
     logger.info("Merge detected")
-    registry_data_dir = os.getenv('REGISTRY_DATA_DIR',
-                                  "/var/opt/gitlab/gitlab-rails/shared/registry/docker/registry/v2")
     this_repo_image = "%s/%s" % (project_namespace, project_name)
-
-    untagged = False
-    prune = True
     # find all images
     images = [this_repo_image]
-    for i in os.listdir(f"{registry_data_dir}/repositories/{this_repo_image}"):
-        if i not in ['_layers', '_manifests', '_uploads']:
-            images.append(f"{this_repo_image}/{i}")
+    try:
+        for i in os.listdir(f"{REGISTRY_DATA_DIR}/repositories/{this_repo_image}"):
+            if i not in ['_layers', '_manifests', '_uploads']:
+                images.append(f"{this_repo_image}/{i}")
+    except Exception as e:
+        print(e)
     # remove all images with this tag
     for image in images:
-        logger.info("Trying to delete %s:%s" % (image, tag))
-        try:
-            cleaner = delete_docker_registry_image.RegistryCleaner(registry_data_dir, dry_run)
-            if untagged:
-                cleaner.delete_untagged(image)
-            else:
-                if tag:
-                    cleaner.delete_repository_tag(image, tag)
-                else:
-                    cleaner.delete_entire_repository(image)
+        remove(image, tag)
 
-            if prune:
-                cleaner.prune()
-        except delete_docker_registry_image.RegistryCleanerError as error:
-            logger.fatal(error)
+def remove(image, tag):
+    untagged = False
+    prune = True
+    logger.info("Trying to delete %s:%s" % (image, tag))
+    try:
+        cleaner = delete_docker_registry_image.RegistryCleaner(REGISTRY_DATA_DIR, dry_run)
+        if untagged:
+            cleaner.delete_untagged(image)
+        else:
+            if tag:
+                cleaner.delete_repository_tag(image, tag)
+            else:
+                cleaner.delete_entire_repository(image)
+
+        if prune:
+            cleaner.prune()
+    except delete_docker_registry_image.RegistryCleanerError as error:
+        logger.fatal(error)
 
 
 app = Application()
 router = app.router
-router.add_route('/{project_namespace}/{project_name}/{tag}', validate)
+router.add_route('/{project_namespace}/{project_name}/{tag}', batch_remove)
+router.add_route('/extra_path', single_remove)
 app.run(host='0.0.0.0', port=80, debug=True)
